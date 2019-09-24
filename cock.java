c00379f388de325b5d60345e6e5dbb9b1ecc5882
samy-b <55136342+samy-b@users.noreply.github.com>
2019-09-24 14:43:11 +0100

    Create cock.java

---
+++ cock.java
@@ -0,0 +1,24 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("World hi");
+  james newvar = new cock ();
+  newvar.printMessage();
+  }
+}
+cock.java
+public class cock{
+  public cock(){
+ System.out.println("I have just printed out cock");
+ System.out.println("i have just printed out twat");
+  }
+
+  public void printMessage(){
+    System.out.println("I am a message");
+  }
+}
+  public
+    public void sayName(String name) {
+    for (int i = 1; i <= 100; i++) {
+      System.out.println(name + " " + i);
+    }
+  }  
